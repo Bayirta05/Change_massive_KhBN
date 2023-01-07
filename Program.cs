1cb61e2266e60a5d0165e404da42c5cd45a59d97
Bayrata <shinshou861105@gmail.com>
2023-01-07 21:06:13 +0300

    Third commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿string[] a=new string[5] {"123", "cdaas", "def", "12365", "-1"};
+string[] b=new string[a.Length];
+void NewArray(string[] a, string[] b)
+{
+    int j=0;
+    for (int i=0;i<a.Length;i++)
+    {
+        if (a[i].Length<=3)
+        {
+            b[j]=a[i];
+            j++;
+        }
+    } 
+}
+
+void PrintArray(string[] b)
+{
+    for (int i=0;i<b.Length;i++)
+    {
+        Console.Write($"{b[i]} ");
+    }
+    Console.WriteLine();
+}
+
+NewArray(a, b);
+PrintArray(a);
+System.Console.WriteLine();
+PrintArray(b);
